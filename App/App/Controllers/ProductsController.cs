bb61be382a2646cc50c024e6fce9fe08d38b4183
yahia199 <yahiakhlil0151400@gmail.com>
2022-06-26 00:30:55 +0300

    AddSearchBar

--- App/App/Controllers/ProductsController.cs
+++ App/App/Controllers/ProductsController.cs
@@ -211,6 +211,22 @@ namespace App.Controllers
 
             return Redirect(urlAnterior);
         }
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allProducts = await _product.GetProducts();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResultNew = allProducts.Where(y => y.Name.Contains(searchString)).ToList();
+
+                return View("Index", filteredResultNew);
+            }
+
+            return View("Index", allProducts);
+        }
+
+
+
 
 
 
